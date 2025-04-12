c21f8f7ad8cdc29b9f3c8780228312a569d02b58
satorutoji <ezdummyacc@gmail.com>
2025-04-12 17:46:39 +0500

    <K extends keyof typeof state> im just in shock

--- src/app/pages/hello/page.tsx
+++ src/app/pages/hello/page.tsx
@@ -1,7 +1,4 @@
-import { UserCard } from '@/components/cards/UserCard'
 import { Nav } from '@/components/Nav'
-import Image from 'next/image'
-import Link from 'next/link'
 import { UserPortrain } from './UserPortrain'
 import { UserStarts } from './UserStarts'
 
--- src/components/cards/UserCard.tsx
+++ src/components/cards/UserCard.tsx
@@ -2,95 +2,90 @@
 
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
-import { Nav } from '../Nav'
 
 export function UserCard() {
-    const [age, setAge] = useState<number>(0)
-    const [city, setCity] = useState<string>('2')
-    const [gender, setGender] = useState<string>('male')
+    const [state, setState] = useState({
+        age: 0,
+        city: '2',
+        gender: 'male',
+    })
     const router = useRouter()
 
-    function handleAgeChange(event: React.ChangeEvent<HTMLInputElement>) {
-        setAge(Number(event.target.value))
+    function handleChange<K extends keyof typeof state>(
+        key: K,
+        value: (typeof state)[K]
+    ) {
+        setState((prev) => ({ ...prev, [key]: value }))
     }
-
-    function handleCityChange(event: React.ChangeEvent<HTMLSelectElement>) {
-        setCity(event.target.value)
-    }
-
-    function handleGenderChange(event: React.ChangeEvent<HTMLInputElement>) {
-        setGender(event.target.value)
-    }
-
     return (
-        <>
-            <form id="form">
-                <fieldset>
-                    <legend>1. Your Gender:</legend>
-                    <div>
-                        <input
-                            type="radio"
-                            id="male"
-                            name="gender"
-                            value="male"
-                            // checked={gender === 'male'}
-                            onChange={handleGenderChange}
-                        />
-                        <label htmlFor="male">male</label>
-                    </div>
-                    <div>
-                        <input
-                            type="radio"
-                            id="female"
-                            name="gender"
-                            value="female"
-                            checked={gender === 'female'}
-                            onChange={handleGenderChange}
-                        />
-                        <label htmlFor="female">female</label>
-                    </div>
-                </fieldset>
-
-                <fieldset>
-                    <legend>2. Your City:</legend>
-                    <select
-                        name="city"
-                        id="city"
-                        value={city}
-                        onChange={handleCityChange}
-                    >
-                        <option value="1">Москва</option>
-                        <option value="2">Санкт-Петербург</option>
-                        <option value="3">Екатеринбург</option>
-                        <option value="4">Калининград</option>
-                    </select>
-                </fieldset>
-
-                <fieldset>
-                    <legend>3. How old are you?</legend>
-                    <label htmlFor="age">ur age:</label>
+        <form id="form">
+            <fieldset>
+                <legend>1. Your Gender:</legend>
+                <div>
                     <input
-                        type="range"
-                        id="age"
-                        min="0"
-                        max="9"
-                        value={age}
-                        onChange={handleAgeChange}
+                        type="radio"
+                        id="male"
+                        name="gender"
+                        value="male"
+                        checked={state.gender === 'male'}
+                        onChange={(e) => handleChange('gender', e.target.value)}
                     />
-                    <span id="res">{age}</span>
-                </fieldset>
-                <a href="#IqTestDescription">Continue</a>
-                <button
-                    type="submit"
-                    onClick={(e) => {
-                        e.preventDefault()
-                        router.push('/pages/iqTest')
-                    }}
-                    className='absolute m-auto right-0 left-0 w-fit'
+                    <label htmlFor="male">male</label>
+                </div>
+                <div>
+                    <input
+                        type="radio"
+                        id="female"
+                        name="gender"
+                        value="female"
+                        checked={state.gender === 'female'}
+                        onChange={(e) => handleChange('gender', e.target.value)}
+                    />
+                    <label htmlFor="female">female</label>
+                </div>
+            </fieldset>
+
+            <fieldset>
+                <legend>2. Your City:</legend>
+                <select
+                    name="city"
+                    id="city"
+                    value={state.city}
+                    onChange={(e) => handleChange('city', e.target.value)}
                 >
-                    Continue
-                </button>
-            </form>
-        </>
+                    <option value="1">Москва</option>
+                    <option value="2">Санкт-Петербург</option>
+                    <option value="3">Екатеринбург</option>
+                    <option value="4">Калининград</option>
+                </select>
+            </fieldset>
+
+            <fieldset>
+                <legend>3. How old are you?</legend>
+                <label htmlFor="age">ur age:</label>
+                <input
+                    type="range"
+                    id="age"
+                    min="0"
+                    max="9"
+                    value={state.age}
+                    onChange={(e) =>
+                        handleChange('age', Number(e.target.value))
+                    }
+                />
+                <span id="res">{state.age}</span>
+            </fieldset>
+
+            <button
+                type="submit"
+                onClick={(e) => {
+                    e.preventDefault()
+                    router.push('/pages/hello#IqTestDescription')
+                }}
+                className="absolute m-auto right-0 left-0 w-fit"
+            >
+                Continue
+            </button>
+        </form>
     )
 }
